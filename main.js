300cf30f84eb7267f087e4f39be7e75664ebe844
Vishesh <visheshsingh16@gmail.com>
2018-01-13 10:39:50 -0600

    Efficient use of 'filter' function

--- main.js
+++ main.js
@@ -17,8 +17,49 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //     console.log(companies[i]);
 // }
 
+
+//GET 21 and older
 // forEach loop
-companies.forEach(function(company){
-    console.log(company);
-    console.log(company.name);
-});
+// companies.forEach(function(company){
+//     console.log(company);
+//     console.log(company.name);
+// });
+
+// filtering using for loop
+// let canDrink = [];
+// for(let i=0;i<ages.length;i++){
+//     if(ages[i] >= 21){
+//         canDrink.push(ages[i]);
+//     }
+// }
+
+// filtering using filter function
+// const canDrink = ages.filter(function(age){
+//     return (age>=21);
+// });
+
+// filtering using filter by ES6 arrow function
+const canDrink = ages.filter(age => age >=21);
+
+console.log(canDrink);
+
+// Filter the retail companies
+// const retail = companies.filter(function(company){
+//     if(company.category === 'Retail'){
+//         return true;
+//     }
+// });
+
+// Filter the retail companies using arrow functions
+const retail = companies.filter(company => company.category === 'Retail');
+
+console.log(retail);
+
+// Get 90s companies
+const eigtyCompanies = companies.filter(company => company.start >= 1980 && company.start <1990);
+
+console.log(eigtyCompanies);
+
+// Get the companies that latest atleast 10 years
+const decadeOld = companies.filter(company => (company.end - company.start) >= 10);
+console.log(decadeOld);
